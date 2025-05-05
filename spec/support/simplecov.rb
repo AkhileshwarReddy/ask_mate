0e42a405ddff423c93d03a195206fd4df58dfd9a
Akhileshwar Reddy <akhil.vedas@outlook.com>
2025-05-05 16:50:09 +0000

    Skip default application files from simplecov

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -3,4 +3,5 @@ require 'simplecov'
 SimpleCov.start 'rails' do
     coverage_dir 'coverage'
     add_filter '/spec/'
+    add_filter %r{^/app/(controllers|models|jobs|mailers|channels)/application_.*\.rb$}
 end
