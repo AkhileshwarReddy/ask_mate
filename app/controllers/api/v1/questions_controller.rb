d34ed8ecb054017617ce67095190a407601231ea
Akhileshwar Reddy <akhil.vedas@outlook.com>
2025-05-06 18:31:35 +0000

    Implement create, update and destroy APIs for questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,12 +1,15 @@
 module Api
     module V1
         class QuestionsController < BaseController
+            after_action :expire_question_from_cache, only: %i[update destroy]
+            after_action :expire_index_cache, only: %i[create update destroy]
+
             def index
                 result = ::Questions::FetchIndex.new(params).call
 
                 render_success(
                     serializer: nil,
-                    message: "Questions fetched succesfully",
+                    message: "Questions fetched successfully",
                     meta: result[:meta]
                 ) { result[:data] }
             end
@@ -15,8 +18,50 @@ module Api
                 result = ::Questions::FetchShow.new(params).call
 
                 render_success(
-                    message: "Question fetched sucessfully"
+                    message: "Question fetched successfully"
                 ) { result[:data] }
+            end
+
+            def create
+                question = Question.create!(question_params)
+                serializer = QuestionSerializer.new(question)
+
+                render_success(
+                    serializer: serializer,
+                    message: "Question created successfully",
+                    status: :created
+                )
+            end
+
+            def update
+                question = Question.find(params[:id])
+                question.update!(question_params)
+                serializer = QuestionSerializer.new(question)
+
+                render_success(
+                    serializer: serializer,
+                    message: "Question updated successfully"
+                )
+            end
+
+            def destroy
+                Question.destroy!(params[:id])
+
+                head :no_content
+            end
+
+            private
+
+            def question_params
+                params.require(:question).permit(:title, :body, :status)
+            end
+
+            def expire_index_cache
+                expire_matched_cache("questions/page=*")
+            end
+
+            def expire_question_from_cache
+                expire_cache("question/#{params[:id]}")
             end
         end
     end
